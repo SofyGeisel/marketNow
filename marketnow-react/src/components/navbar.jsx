b97e285b1107186123eff3ccb02bd9f0ea4b7a1c
SofyGeisel <sofiafigueroageisel@gmail.com>
2023-04-25 23:29:12 -0400

    NavBar lista

--- marketnow-react/src/components/navbar.jsx
+++ marketnow-react/src/components/navbar.jsx
@@ -1,51 +1,93 @@
-import React from 'react'
-import styled from 'styled-components'
-import SearchIcon from '@mui/icons-material/Search';
-
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Container = styled.div`
-    height: 60px;
-`
+  height: 60px;
+`;
 
 const Wrapper = styled.div`
-    padding: 10px 20px;
-    display: flex;
-    justify-content: space-between;
-`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const Left = styled.div`
-    flex: 1;`
+  flex: 1;
+  display: flex;
+  align-item: center;
+`;
 
 const Language = styled.span`
-    font-size: 14px;
-    cursor: pointer;`
+  font-size: 14px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
 
 const SearchContainer = styled.div`
-    border: 1px solid lightgray;`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
 
 const Center = styled.div`
-    flex: 1;`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
 const Right = styled.div`
-    flex: 1;`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
 
 const navbar = () => {
   return (
     <Container>
-       <Wrapper>
+      <Wrapper>
         <Left>
-            <Language>ES</Language>
-            <SearchContainer>
-                input
-                <SearchIcon/>
-            </SearchContainer>
-            </Left>
-        <Center>center</Center>
-        <Right>right</Right>
-        </Wrapper> 
-      
+          <Language>ES</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{color: "gray", fontSize:16}} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>MarketNow</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTRARSE</MenuItem>
+          <MenuItem>INICIAR SESION</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon color="action" />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default navbar
-
+export default navbar;
--- marketnow-react/src/pages/Home.jsx
+++ marketnow-react/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import Anuncios from '../components/Anuncios'
 
 
 const Home = () => {
   return (
     <div>
+      <Anuncios />
       <Navbar />
     </div>
   )
