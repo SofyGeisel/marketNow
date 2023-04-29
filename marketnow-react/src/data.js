0eaa67538df013939d54f84a159a0210cab2f7ae
SofyGeisel <sofiafigueroageisel@gmail.com>
2023-04-28 20:01:16 -0400

    Slider listo

---
+++ marketnow-react/src/data.js
@@ -0,0 +1,20 @@
+
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: require('./img/audifonos.png'),
+        title: "Aprovecha nuestras ofertas en tecnología",
+    },
+    {
+        id: 2,
+        img: require('./img/sillahuevo.png'),
+        title: "Especial Decoración. 2x1 en Grandes Marcas",
+    },
+    {
+        id: 3,
+        img: require('./img/instrumentos.png'),
+        title: "Hasta un 40% de descuento en Instrumentos musicales",
+    },
+
+];
