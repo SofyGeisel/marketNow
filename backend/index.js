4db30516542a667e6ecf539718dc7fd634b27be3
Marco Gallardo <107008803+MarcoLinkMG@users.noreply.github.com>
2023-05-08 00:11:49 -0400

    Creacion de Backend

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require("jsonwebtoken")
+const { addUser, validarCredenciales } = require('./consultas')
+const { verificarCredenciales, verificarToken } = require('./middelware')
+const { secretKey } = require("./secretkey")
+
+
+app.listen(3000, console.log("Servidor encendido"))
+app.use(express.json())
+app.use(cors())
+
+
+app.post("/usuarios",verificarCredenciales, async (req, res) => {
+
+    try{
+        const { usuarioid = 1, nombre, email, direccion, password, lastloggin } =req.body
+        await addUser(usuarioid, nombre, email, direccion, password, lastloggin)
+        res.send("Usuario ha sido creado con exito")
+    } catch(err){
+        console.log(err)
+        res.status(500).send(err)
+    }
+
+})
+
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        await validarCredenciales(email, password)
+        const token = jwt.sign({ email }, secretKey)
+        res.send(token)
+
+    } catch (error) {
+        console.log(error)
+        res.status(error.code || 500).send(error)
+    }
+})
+
+app.get("/usuarios",verificarToken, async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        res.status(error.code || 500).send(error)
+    }   
+})
