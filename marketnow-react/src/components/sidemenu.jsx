fd86aad5b69129bf748584ff20fa4dc78fcf678a
RodrigoA <rodalarcon@gmail.com>
2023-05-08 23:03:34 -0400

    avances menu lateral

---
+++ marketnow-react/src/components/sidemenu.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+
+const drawerWidth = 240;
+
+const SideMenu = () => {
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+   return(
+    <Box sx={{ width: '100%', maxWidth: 360, height: '100vh',  bgcolor: 'background.paper', marginLeft: 0}}>
+    <List component="nav" aria-label="main mailbox folders">
+      <ListItemButton
+        selected={selectedIndex === 0}
+        onClick={(event) => handleListItemClick(event, 0)}
+      >
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Inbox" />
+      </ListItemButton>
+      <ListItemButton
+        selected={selectedIndex === 1}
+        onClick={(event) => handleListItemClick(event, 1)}
+      >
+        <ListItemIcon>
+          <DraftsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Drafts" />
+      </ListItemButton>
+    </List>
+    
+  </Box>
+    )
+}
+
+export default SideMenu;
--- marketnow-react/src/pages/Miperfil.jsx
+++ marketnow-react/src/pages/Miperfil.jsx
@@ -2,13 +2,30 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Anuncios from '../components/Anuncios'
 import Perfil from '../components/perfil'
+import SideMenu from '../components/sidemenu'
+import Grid from '@mui/material/Grid';
+
+import { Container } from '@mui/material'
 
 const Miperfil = () => {
   return (
-    <div>
+    <div className='contPaginas'>
       <Navbar/>
       <Anuncios/>
-      <Perfil/>
+
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <SideMenu/>
+        </Grid>
+
+        <Grid xs={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Perfil/>
+        </Grid>
+        
+        
+      </Grid>
+      
+      
     </div>
   )
 }
