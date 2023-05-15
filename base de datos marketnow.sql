28ac4a9813c058e5bde09f8458362098f824d24b
Marco Gallardo <107008803+MarcoLinkMG@users.noreply.github.com>
2023-05-15 11:10:22 -0400

    actualizacion de BD

--- base de datos marketnow.sql
+++ base de datos marketnow.sql
@@ -63,11 +63,11 @@ references productos(productoid)
 );
 
 INSERT INTO usuarios(nombre, mail, direccion, password)
-VALUES ('Marco Gallardo', 'mgallardo@linkmg.cl', 'Rio Tolten 2487', '123456');
+VALUES ('Marco Gallardo', 'mgallardo@linkmg.cl', 'Rio Tolten 2487', '$2a$10$5AGI1DNzexxLWMBK1h5IU.arUwetFuTZTibHL/AlUxy8YG1xaXXZe');
 INSERT INTO usuarios(nombre, mail, direccion, password)
-VALUES ('Sofia Figueroa', 'sofiafigueroageisel@gmail.com', 'Viña del mar', '123456');
+VALUES ('Sofia Figueroa', 'sofiafigueroageisel@gmail.com', 'Viña del mar', '$2a$10$5AGI1DNzexxLWMBK1h5IU.arUwetFuTZTibHL/AlUxy8YG1xaXXZe');
 INSERT INTO usuarios(nombre, mail, direccion, password)
-VALUES ('Rodrigo Alarcon', 'rodalarcon@gmail.com', 'Santiago', '123456');
+VALUES ('Rodrigo Alarcon', 'rodalarcon@gmail.com', 'Santiago', '$2a$10$5AGI1DNzexxLWMBK1h5IU.arUwetFuTZTibHL/AlUxy8YG1xaXXZe');
 
 
 INSERT INTO categorias(nombre, descripcion)
@@ -79,12 +79,12 @@ VALUES ('Deporte', 'En esta seccion podras encontrar lo mejor en deportes public
 
 
 INSERT INTO productos(nombre, descripcion, imagen, usuarioid, categoriaid, precio, fecha_publicacion)
-VALUES ('Notebook Toshiba 235', 'Notebook Toshiba Dynabook Tecra Celeron 14 4GB/128GB', './img/laptoptoshiba1.png', '1', '1', 349900, CURRENT_DATE);
+VALUES ('Notebook Toshiba 235', 'Notebook Toshiba Dynabook Tecra Celeron 14 4GB/128GB', './img/laptoptoshiba1.png', '1', '1', 349900, '23-04-2023');
 INSERT INTO productos(nombre, descripcion, imagen, usuarioid, categoriaid, precio, fecha_publicacion)
-VALUES ('Notebook Asus Gaming', 'Notebook TUF Gaming F15 FX506LHB-HN326W Intel Core i5 NVIDIA GeForce GTX 1650 8GB RAM 512GB SSD 15.6', './img/LaptopAsus1.png', '2', '1', 729990, CURRENT_DATE);
+VALUES ('Notebook Asus Gaming', 'Notebook TUF Gaming F15 FX506LHB-HN326W Intel Core i5 NVIDIA GeForce GTX 1650 8GB RAM 512GB SSD 15.6', './img/LaptopAsus1.png', '2', '1', 729990, '04-04-2023');
 INSERT INTO productos(nombre, descripcion, imagen, usuarioid, categoriaid, precio, fecha_publicacion)
-VALUES ('Laptop Dell Vostro', 'Notebook Dell Vostro 3400 Intel Core i5-1135G7 / 8GB RAM / 256GB SSD / 14.0', './img/LaptopDell1.png', '3', '1', 399990, CURRENT_DATE);
+VALUES ('Laptop Dell Vostro', 'Notebook Dell Vostro 3400 Intel Core i5-1135G7 / 8GB RAM / 256GB SSD / 14.0', './img/LaptopDell1.png', '3', '1', 399990, '25-03-2023');
 INSERT INTO productos(nombre, descripcion, imagen, usuarioid, categoriaid, precio, fecha_publicacion)
-VALUES ('Notebook HP 348', 'Notebook Hp 348 G7 I5-10210U 8GB SSD 256GB W10 Home', './img/LaptopHP1.png', '1', '1', 469990, CURRENT_DATE);
+VALUES ('Notebook HP 348', 'Notebook Hp 348 G7 I5-10210U 8GB SSD 256GB W10 Home', './img/LaptopHP1.png', '1', '1', 469990, '26-03-2023');
 INSERT INTO productos(nombre, descripcion, imagen, usuarioid, categoriaid, precio, fecha_publicacion)
-VALUES ('Notebook HP 348 Rojo', 'Notebook Hp 348 G7 I5-10210U 8GB SSD 256GB W10 Home', './img/LaptopHP2.png', '2', '1', 469990, CURRENT_DATE);
+VALUES ('Notebook HP 348 Rojo', 'Notebook Hp 348 G7 I5-10210U 8GB SSD 256GB W10 Home', './img/LaptopHP2.png', '2', '1', 469990, '27-03-2023');
