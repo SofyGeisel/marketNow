a5cc2cfb51925c61f6cd47bc8c3a68b6ca11ea47
SofyGeisel <sofiafigueroageisel@gmail.com>
2023-05-26 17:52:19 -0400

    Update Producto.jsx

--- marketnow-react/src/components/Producto.jsx
+++ marketnow-react/src/components/Producto.jsx
@@ -139,7 +139,7 @@ const verProducto = `/detalleproducto/${prodId}`
   return (
       <Container>
         <FondoImagen>
-        <Image src={item.imagen}/>
+        <Image src={"require( + {item.imagen} + )"}/>
         </FondoImagen>
         <TituloyDescripcion>
         <Titulo>{item.nombre}</Titulo> 
