445199278e43f737e39becb994989725fff17804
Marco Gallardo <107008803+MarcoLinkMG@users.noreply.github.com>
2023-05-28 15:20:36 -0400

    Update DetalleCompraComponent.jsx

--- marketnow-react/src/components/DetalleCompraComponent.jsx
+++ marketnow-react/src/components/DetalleCompraComponent.jsx
@@ -20,7 +20,7 @@ const DetalleCompraComponent = () => {
   });*/
 
 const traerProductosCompras = async () => {
-  const response = await fetch(`http://localhost:3000/comprasdetalle/${id}`, {
+  const response = await fetch(`http://localhost:3000/comprasdetalle/8`, {
     method: "GET", // or 'PUT'
     headers: {
       "Content-Type": "application/json",
