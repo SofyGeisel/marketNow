f7dfb23031ee5f9d47ca56e5ac824178ad6a8bdb
Marco Gallardo <107008803+MarcoLinkMG@users.noreply.github.com>
2023-05-28 22:58:35 -0400

    listo mis compras

--- marketnow-react/src/components/ItemMisCompras.jsx
+++ marketnow-react/src/components/ItemMisCompras.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Box, Button } from "@mui/material";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import ContextProductos from "../contextProductos";
 
 const CustomButton = styled(Button)`
   && {
@@ -22,22 +24,24 @@ const CustomButton = styled(Button)`
 const ItemMisCompras = ({ item }) => {
 
     const navigate = useNavigate();
-    const precioProducto = parseInt(item.total)
-    const precioFormato = precioProducto.toLocaleString('eng', {
+    const { prodIdCompras, setprodIdCompras } = useContext(ContextProductos)
+    const precioTotal = parseInt(item.total)
+    const precioFormato = precioTotal.toLocaleString('eng', {
         style: 'currency',
         currency: 'USD',
         maximumFractionDigits: 0
     })
 
-    const verDetalle = () => {
-      
-      navigate(`/detallecompra/${item.compraid}`);
-    
+    const id = item.compraid
+    const verdetalle = () => {
+      setprodIdCompras(id)
+      navigate(`/detallecompra/${id}`)
+
     }
     
 
     return(
-        <>
+        <div>
             <Box sx={{ boxShadow: 1, bgcolor: "white", p: 2, m: 2, borderRadius: 2,display: "flex", flexDirection: "row", alignContent: "space-around"  }}>
                 
                 <div className="detalleCarrito">
@@ -45,9 +49,9 @@ const ItemMisCompras = ({ item }) => {
                     <p><span>ID de compra: </span>{item.compraid}</p>
                     <p><span>Total: </span>{precioFormato}</p>
                 </div>
-                <CustomButton variant="contained" sx={{alignSelf:"center"}} onClick={verDetalle}>Ver detalle</CustomButton>
+                <CustomButton variant="contained" sx={{alignSelf:"center"}} onClick={verdetalle}>Ver detalle</CustomButton>
             </Box>
-        </>
+        </div>
     )
 }
 
